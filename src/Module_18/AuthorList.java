82a514eb8aa99f642b6abb55c3eb4c5b9ad3ee46
Thabo <vollyegen@gmail.com>
2016-05-05 14:20:45 +0200

    Initial checking of books

---
+++ src/Module_18/AuthorList.java
@@ -0,0 +1,73 @@
+package Module_18;
+
+public class AuthorList 
+{
+	//access level to data members 
+	 private String fname,lname;
+	
+	 //methods filled with display functions
+		public void selAuthor()
+		{
+			System.out.println("____[AUTHORS]_____");
+			System.out.println("1. Robert Kiyosaki");
+			System.out.println("2. John C Maxwell");
+			System.out.println("3. Donald Trump");
+			System.out.println("");
+		}
+		
+		public void showBook()
+		{
+			System.out.println("1. Rich Dad,Poor Dad");
+			System.out.println("2. Guide to Investing");
+		}
+		
+		//overloaded methods
+		public int gti(int a , int b,int c)
+		{	
+			a = 170;
+			c = a-b;		
+			return c;	
+		}
+		//utility methods
+		public int rdpd(int a , int b,int c)
+		{
+			a= 130;
+			c = a-b;		
+			return c;	
+		}
+	 
+	 
+	 /**
+	    * Class constructor for customer name.
+	    */
+	 public AuthorList()
+		{
+			
+		}
+	 
+	 //Encapsulation
+	 public AuthorList(String fname,String lname)
+		{
+			this.fname= fname;
+			this.lname=lname;
+		}
+	 //getter methhod
+	public String getfName() 
+	{
+        return fname;
+    }
+	//setter method
+    public void setfName(String f_name)
+    {
+        this.fname = f_name;
+    }
+    public String getlName() {
+        return lname;
+    }
+    public void setlName(String l_name) 
+    {
+        this.lname =l_name;
+    }    
+        
+	
+}
---
+++ src/Module_18/Driver.java
@@ -0,0 +1,100 @@
+package Module_18;
+
+import java.util.Scanner;
+import java.util.Date;
+
+
+/**
+ * driver class containing main method
+ */
+public class Driver{
+
+	//access methods
+	private static int c;
+	private static int a;
+	private static String f, l;
+	
+	public static void main(String[] args)
+	{
+		//object creation
+		AuthorList al = new AuthorList ();
+		String author;
+		int sel=0;
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out .println("Enter your first name");
+		f = sc.nextLine();
+		System.out .println("Enter your last name");
+		l = sc.next();
+		System.out.println("");
+		
+		//call method
+		al.selAuthor();
+		
+	    System.out.println("SELECT NUMBER ASSIGNED TO REQUIRED AUTHOR");
+	    sel = sc.nextInt();
+
+	    if ( sel== 1)
+	    {
+		   
+	    	author = "Robert Kiyosaki";
+	    	System.out.println(author+"'s available books");
+	    	System.out.println("");	    	
+	    	
+	    	al.showBook();
+	    	System.out.println("");
+	    	System.out.println("SELECT NUMBER ASSIGNED TO REQUIRED BOOK");
+	    	  	
+		
+	    }
+	    else if (sel == 2)
+	    {
+	        author = "John C Maxwell";
+	        System.err.println("No units available for "+author);
+	    } 
+	    else if (sel == 3) 
+	    {
+	       author = "Donald Trump";
+	       System.err.println("No units available for "+author);
+	    }
+	    else 
+	    {
+	        System.err.println("invalid input");    
+	    }
+	    
+	    sel = sc.nextInt();
+	   
+	    if (sel == 1)
+	    {
+		System.out.println("Rich Dad,Poor Dad Costs R130");
+		System.out.println(""); 
+		System.out.println("Please enter deposit amount");
+		sel = sc.nextInt();
+	
+		System.out.println("OUTSTANDING AMOUNT\t:R"+al.rdpd(a ,sel,c));
+		System.out.println("CUSTOMER NAME\t\t:"+ f+" "+l );
+		Date date=new Date(); 
+		System.out.println("DATE OF PURCHASE\t:"+date);
+		
+	    }
+	    else if (sel == 2) 
+	    {
+		System.out.println("Guide to investing costs R170");   
+		System.out.println(""); 
+		System.out.println("Please enter deposit amount");
+		sel = sc.nextInt();	
+		System.out.println("OUTSTANDING AMOUNT\t:R"+al.gti(a ,sel,c));
+		System.out.println("CUSTOMER NAME\t\t:"+ f+" "+l );
+		Date date=new Date(); 
+		System.out.println("DATE OF PURCHASE\t:"+date);
+	   }
+	   else
+	   {
+		   System.err.println("Invalid Selection");  
+	   }
+	   
+	   
+	}
+
+}
